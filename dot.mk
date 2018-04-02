f44bedbff321ebf4619c656d11a2840ecdf3deae
Sagar Makhar <sagarmakhar@gmail.com>
2018-04-02 20:17:51 +0000

    s2: DotOS bring-up

--- dot.mk
+++ dot.mk
@@ -6,11 +6,11 @@ TARGET_SCREEN_HEIGHT := 1920
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
-PRODUCT_NAME := lineage_s2
+PRODUCT_NAME := dot_s2
 PRODUCT_BRAND := LeEco
 PRODUCT_MANUFACTURER := LeMobile
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_s2-userdebug
+add_lunch_combo dot_s2-userdebug
